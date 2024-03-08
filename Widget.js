d808337991106a6b1cf423cbf4824bf52f7ae602
JGutierrez-EsriChile <116380714+JGutierrez-EsriChile@users.noreply.github.com>
2024-03-08 15:40:51 -0300

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -95,7 +95,7 @@ function(declare, Query, QueryTask, domConstruct, array, lang, query, on, Deferr
       console.log('postOpenSectoresAP');
       var that = this;
       extractData = this.extractData;
-      this.Clear = fn_Limpiar
+      Clear = fn_Limpiar
       Sectorizar = fn_Sectorizar;
       radioSelect = funcionRadioSelect;
 
