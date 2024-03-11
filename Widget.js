f8afeec6d60c938042cda35a98809e8d4a84cc62
JGutierrez-EsriChile <116380714+JGutierrez-EsriChile@users.noreply.github.com>
2024-03-11 16:56:02 -0300

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -295,8 +295,7 @@ function(declare, Query, QueryTask, domConstruct, array, lang, query, on, Deferr
             that.resaltarAPEnMapa(ft, [255,0,255], 16);
             that.resumenSectorAP();
           })
-          console.log("extent: ",extent)
-          that.map.setExtent(extent);
+          // that.map.setExtent(extent);
         });
       })
       // that.resumenSectorAP();
