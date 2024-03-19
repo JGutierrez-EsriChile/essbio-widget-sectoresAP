e35bdd2a49423ab5f856a2cab657e8a81168c47a
JGutierrez-EsriChile <116380714+JGutierrez-EsriChile@users.noreply.github.com>
2024-03-19 18:49:15 -0300

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -347,11 +347,13 @@ function(declare, Query, QueryTask, domConstruct, array, lang, query, on, Deferr
         var systemsubnetwork = featureSet.attributes["systemsubnetworkname"];
         var pressuresubnetwork = featureSet.attributes["pressuresubnetworkname"];
         var isolationsubnetwork = featureSet.attributes["isolationsubnetworkname"];
+        console.log ("350",assetid, systemsubnetwork,pressuresubnetwork, isolationsubnetwork);
         slct_UTILITY(assetid, systemsubnetwork,pressuresubnetwork, isolationsubnetwork);
         
         var codigo_sector_distribucion = featureSet.attributes["codigo_sector_distribucion"];
         var codigo_sector_presion = featureSet.attributes["codigo_sector_presion"];
         var codigo_cuartel = featureSet.attributes["codigo_cuartel"];
+        console.log ("356",assetid, codigo_sector_distribucion, codigo_sector_presion, codigo_cuartel);
         slct(assetid,codigo_sector_distribucion,codigo_sector_presion, codigo_cuartel);
 
         document.getElementById("zoneInfo").innerHTML = "";
